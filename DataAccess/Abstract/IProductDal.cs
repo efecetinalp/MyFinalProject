a2fba19aacf553ba2d87503538db59d4bc8d5877
efecetinalp <efecetinalp@gmail.com>
2022-09-11 18:24:32 +0300

    business layer updated

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -10,6 +10,7 @@ namespace DataAccess.Abstract
     public interface IProductDal
     {
         List<Product> GetAll();
+        List<Product> GetAllByCategory(int categoryId);
         void Add(Product product);
         void Update(Product product);
         void Delete(Product product);
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -8,26 +8,49 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.InMemory
 {
-    internal class InMemoryProductDal : IProductDal
+    public class InMemoryProductDal : IProductDal
     {
+        List<Product> _products;
+        
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName= "Laptop", UnitPrice=1000, UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName= "Camera", UnitPrice=800, UnitsInStock=10},
+                new Product{ProductId=3, CategoryId=2, ProductName= "Table", UnitPrice=200, UnitsInStock=35},
+                new Product{ProductId=4, CategoryId=2, ProductName= "Chair", UnitPrice=80, UnitsInStock=50},
+                new Product{ProductId=5, CategoryId=2, ProductName= "Lamp", UnitPrice=50, UnitsInStock=22}
+            };
+        }
+
         public void Add(Product product)
         {
-            throw new NotImplementedException();
+            _products.Add(product);
         }
 
         public void Delete(Product product)
         {
-            throw new NotImplementedException();
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
         }
 
         public void Update(Product product)
         {
-            throw new NotImplementedException();
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
         }
     }
 }

850dd5ae01078189afd28c565bf83f2658e4d253
efecetinalp <efecetinalp@gmail.com>
2022-09-21 00:35:00 +0300

    WebAPI added

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product> { 
+                new Product { ProductId=1, CategoryId=1, ProductName ="product1", UnitPrice = 22},
+                new Product{ ProductId=2, CategoryId=2, ProductName ="product2", UnitPrice=33}};
+        }
+    }
+}
